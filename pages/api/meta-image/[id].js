262689e1e3df1f6e14a348df8575826e860f79b1
Samarth Jajoo <jajoosam@gmail.com>
2020-05-19 18:35:36 +0530

    remove meta cache

--- pages/api/meta-image/[id].js
+++ pages/api/meta-image/[id].js
@@ -6,9 +6,7 @@ export default async (req, res) => {
     return res.end();
   }
   res.writeHead(302, {
-    Location: `https://res.cloudinary.com/skrrt/image/fetch/${encodeURIComponent(
-      `https://skrrt-filter-og.now.sh/${req.query.id}/100?w=1200&h=635`
-    )}`,
+    Location: `https://skrrt-filter-og.now.sh/${req.query.id}/100?w=1200&h=635`,
   });
   res.end();
 };
